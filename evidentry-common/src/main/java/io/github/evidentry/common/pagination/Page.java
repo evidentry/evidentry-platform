9d00383236ed9a96512680776a30f94d717c3fb7
Edward L. <edyardlyk@gmail.com>
2025-07-07 19:00:28 +0300

    refactor: update Page.java
    - use fluent naming convention
    - remove `extends Stream`
    - add `io.github.evidentry.common.pagination.Page.stream`
    - add `io.github.evidentry.common.pagination.Page.map`
    
    Signed-off-by: Edward L. <edyardlyk@gmail.com>

--- evidentry-common/src/main/java/io/github/evidentry/common/pagination/Page.java
+++ evidentry-common/src/main/java/io/github/evidentry/common/pagination/Page.java
@@ -1,16 +1,39 @@
 package io.github.evidentry.common.pagination;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public interface Page<T> extends Stream<T> {
-    List<T> getContent();
+public interface Page<T> {
+    List<T> content();
 
-    Integer getNumber();
+    Integer number();
 
-    Integer getSize();
+    default Integer size() {
+        return content() == null ? 0 : content().size();
+    }
 
-    Integer getTotalPages();
+    Integer totalPages();
 
-    Integer getTotalElements();
+    Integer totalElements();
+
+    default Stream<T> stream() {
+        return content() == null ? Stream.empty() : content().stream();
+    }
+
+    default <U> Page<U> map(Function<T, U> mapper) {
+        return stream()
+                .map(mapper)
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        l -> SimplePage.<U>builder()
+                                .content(l)
+                                .number(number())
+                                .size(size())
+                                .totalPages(totalPages())
+                                .totalElements(totalElements())
+                                .build()
+                ));
+    }
 }
