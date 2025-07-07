3c93f519fd0eda31d96e12a28c509497ca679d2f
Edward L. <edyardlyk@gmail.com>
2025-07-07 19:02:25 +0300

    refactor: use spi
    Signed-off-by: Edward L. <edyardlyk@gmail.com>

--- evidentry-core/src/main/java/io/github/evidentry/core/spi/AuditEventService.java
+++ evidentry-core/src/main/java/io/github/evidentry/core/spi/AuditEventService.java
@@ -1,12 +1,11 @@
 package io.github.evidentry.core.spi;
 
-import io.github.evidentry.common.pagination.PageResult;
-import io.github.evidentry.common.pagination.PageRequest;
+import io.github.evidentry.common.pagination.Page;
+import io.github.evidentry.common.pagination.Pagination;
 import io.github.evidentry.core.model.AuditEventAccessor;
-import io.github.evidentry.core.model.AuditEventSearchCriteria;
 
 public interface AuditEventService<E extends AuditEventAccessor> {
     E track(E auditEvent);
 
-    PageResult<E> query(AuditEventSearchCriteria criteria, PageRequest pageRequest);
+    Page<E> query(String entityId, Pagination pagination);
 }
